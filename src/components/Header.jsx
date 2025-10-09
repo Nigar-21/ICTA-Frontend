545adcf4f540710771fca4cbf457e570efe83fc6
Nigar <haciyeva.nigar2004@gmail.com>
2025-10-09 22:48:36 +0400

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -33,13 +33,13 @@ export default function Header() {
           <div key={index} className="relative group">
             {link.dropdown ? (
               <>
-                <span className="cursor-pointer transition-colors duration-200 hover:bg-[#1E3A8A] py-2 px-4  rounded-full ">{link.name}</span>
-                <div className="absolute left-0 top-full mt-2 w-52 bg-white rounded-lg shadow-lg opacity-0 group-hover:opacity-100 invisible group-hover:visible transition-all duration-300 z-10">
+                <span className="cursor-pointer transition-colors duration-200 hover:bg-[#1E3A8A] py-2 px-4  hover:shadow-xl rounded-full ">{link.name}</span>
+                <div className="absolute left-0 top-full mt-2 w-52 bg-white rounded-lg shadow-2xl opacity-0 group-hover:opacity-100 invisible group-hover:visible hover:shadow-xl transition-all duration-300 z-10">
                   {link.dropdown.map((item, idx) => (
                     <Link
                       key={idx}
                       to={item.to}
-                      className="block px-4 py-2 text-gray-700 hover:bg-[#1E3A8A]  hover:text-white transition-colors duration-200"
+                      className="block px-4 py-2 text-gray-700 hover:bg-[#1E3A8A]  hover:text-white hover:shadow-xl transition-colors duration-200"
                     >
                       {item.name}
                     </Link>
@@ -49,7 +49,7 @@ export default function Header() {
             ) : (
               <Link
                 to={link.to}
-                className="relative transition-colors duration-200 hover:bg-[#1E3A8A] py-2 px-4 rounded-full"
+                className="relative transition-colors duration-200 hover:bg-[#1E3A8A] hover:shadow-xl py-2 px-4 rounded-full"
               >
                 {link.name}
               </Link>
--- src/pages/home/main.jsx
+++ src/pages/home/main.jsx
@@ -1,12 +1,64 @@
-import React from 'react'
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import 'react-calendar/dist/Calendar.css';
+import { FaFilePdf, FaVideo, FaQuestionCircle, FaRegFileAlt, FaBook, FaLaptopCode } from "react-icons/fa"; 
+import { useNavigate } from "react-router-dom";
+export default function Main() {
+    
+    const services = [
+    { title: "PDF Təlimatlar", icon: FaFilePdf, route: "/pdf" },
+    { title: "Video Təlimatlar", icon: FaVideo, route: "/video" },
+    { title: "Quizlər", icon: FaQuestionCircle, route: "/quiz" },
+    { title: "Müraciət qaydaları", icon: FaRegFileAlt, route: "/rules" },
+    { title: "Siyasətlər", icon: FaBook, route: "/policies" },
+    { title: "Daxili IT qaydaları", icon: FaLaptopCode, route: "/it-guidelines" },
+  ];
 
-export default function main() {
+  const meetings = [
+  { date: '2025-10-09', time: '06:00 - 07:00', place: 'Ofis' },
+  { date: '2025-10-09', time: '09:00 - 10:00', place: 'Ofis' },
+  { date: '2025-10-09', time: '11:00 - 12:00', place: 'Ofis' },
+   { date: '2025-10-09', time: '13:00 - 14:00', place: 'Ofis' },
+  { date: '2025-10-10', time: '10:00 - 11:00', place: 'Ofis' },
+  { date: '2025-10-10', time: '13:00 - 14:00', place: 'Ofis' },
+];
+
+
+   const [selectedDate, setSelectedDate] = useState(new Date());
+
+
+
+const filteredMeetings = meetings
+  .filter(m => {
+    const meetingDate = new Date(m.date);
+    return (
+      meetingDate.getFullYear() === selectedDate.getFullYear() &&
+      meetingDate.getMonth() === selectedDate.getMonth() &&
+      meetingDate.getDate() === selectedDate.getDate()
+    );
+  })
+  .sort((a, b) => {
+    const hourA = parseInt(a.time.split(':')[0], 10);
+    const hourB = parseInt(b.time.split(':')[0], 10);
+    return hourA - hourB;
+  })
+  .slice(0, 3)
+  .map((m, idx) => ({ ...m, title: `Görüş ${idx + 1}` }));
+
+
+
+  
+  
+
+  
+  
   return (
+    <>
        <div      style={{
     background: 'linear-gradient(180deg, #162556 0%, #2c3a7b 50%, #3b446f 100%)'
   }}
         className="mx-auto flex 
-       p-8 flex-col items-center gap-8  w-full  h-[350px]   ">
+       p-8 flex-col items-center gap-8  w-full  h-[380px] relative  ">
         <h1 className='text-white font-semibold text-[35px] '>Şəbəkə inzibatçılığı və texniki dəstək şöbəsi</h1>
           <div className=" w-[40%] items-center justify-between rounded-full border border-[#D4D4D4] hover:border-[#92B5F6] sm:flex sm:h-[48px] sm:px-6">
             <input
@@ -22,13 +74,87 @@ export default function main() {
             />
             <button className="ml-2  flex-shrink-0 cursor-pointer"> 
            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z" stroke="white" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M22 22L20 20" stroke="white" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z" stroke="white" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"/>
+<path d="M22 22L20 20" stroke="white" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"/>
 </svg>
 
             </button>
           </div>
-          <button className='px-16 py-3.5 bg-[#e6e6e6] text-[#1E3A8A] border border-[#1E3A8A]  font-semibold rounded-full   hover:bg-[#1E3A8A] hover:border-[#1E3A8A] hover:text-white hover:shadow-lg'>Müraciət et</button>
+          <button className='px-16 py-3.5 bg-[#e6e6e6] text-[#1E3A8A] border border-[#1E3A8A]  font-semibold rounded-full   hover:bg-[#1E3A8A] hover:border-[#1E3A8A]  mt-2 hover:text-white hover:shadow-lg'>Müraciət et</button>
         </div>
+
+
+       {/*boxes*/}
+    <section className="bg-transparent text-black pb-6 px-8 relative -mt-14">
+  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto">
+    {services.map((service, index) => (
+      <div  onClick={() => navigate(service.route)} 
+        key={index}
+        className="bg-white rounded-2xl p-6 h-[200px] hover:bg-[#eef2ff] transition-all duration-300 shadow-xl flex flex-col justify-center items-start cursor-pointer border border-[#e5e7eb] relative overflow-visible"
+      >
+        <div className="w-16 h-16 mb-4 flex items-center justify-center rounded-full bg-[#dce9ff] shadow-sm">
+          <service.icon className="text-3xl text-[#1E3A8A]" />
+        </div>
+        <h3 className="text-xl font-semibold">{service.title}</h3>
+
+    
+        <div      className="absolute bottom-10 right-4 w-8 h-8 flex items-center justify-center bg-[#1E3A8A] text-white rounded-full shadow-lg cursor-pointer ">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+          </svg>
+        </div>
+      </div>
+    ))}
+  </div>
+</section>
+
+  {/*calendar*/}
+   <section className="bg-white p-20 rounded-2xl   w-[83%] mx-auto justify-center flex gap-14 ">
+      
+      <div className="">
+        <Calendar
+          onChange={setSelectedDate}
+          value={selectedDate}
+          className="rounded-xl shadow-2xl p-4"
+        />
+      </div>
+
+      {/* Görüşlər */}
+      <div className="w-[60%] flex flex-col gap-4">
+        <div className="flex justify-between items-center">
+          <h3 className="text-[24px] font-semibold text-black">
+            Seçilmiş tarix: {selectedDate.toLocaleDateString()}
+          </h3>
+          <button className="px-4 py-2 bg-[#1E3A8A] text-white rounded-full hover:bg-[#17275B] transition font-medium">
+            Yeni Görüş
+          </button>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          {filteredMeetings.map((meeting, idx) => (
+            <div key={idx} className="bg-white p-4 rounded-xl shadow-lg hover:hover:bg-[#eef2ff] transition cursor-pointer">
+              <h4 className="font-semibold text-[#1E3A8A] text-[18px] mb-1">{meeting.title}</h4>
+              <p className="text-gray-600 text-[14px] text-sm">{meeting.time}</p>
+              <p className="text-gray-500 text-[13px] mt-1">Məkan: {meeting.place}</p>
+            </div>
+          ))}
+        </div>
+
+        {filteredMeetings.length > 0 && (
+          <div className="mt-2 flex justify-end">
+            <button className="px-4 py-2 bg-gray-200 text-[#1E3A8A] font-medium rounded-full hover:bg-gray-300 transition cursor-pointer">
+              Bütün Görüşlər
+            </button>
+          </div>
+        )}
+      </div>
+    </section>
+</>
   )
 }
